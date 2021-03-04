508dd8bc9899ededefecf7cc39b8e6e1c74f0b39
Mason Ellwood <masonellwood@gmail.com>
2021-03-03 20:08:51 -0700

    updated layout

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
+// import react and react dom libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+if (module.hot) {
+    module.hot.accept();
+}
+
+// create a react component
+
+const App = () => {
+    return <div>Hi there!</div>;
+}
+
+// take react component and show it to the screen
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.querySelector('#root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
